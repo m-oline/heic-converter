c5afcfff9ec920e91fe1e1ec9c060388c8d3d786
m-oline <marieolineobergfaurhoj@gmail.com>
2026-05-12 21:25:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from pathlib import Path, PurePath
+from PIL import Image
+import pillow_heif
+
+cwd = Path.cwd()
+input_dir = cwd / "input"
+output_dir = cwd / "output"
+
+def main():
+    Path(output_dir).mkdir(exist_ok=True)
+
+    for entry in input_dir.iterdir():
+        if entry.is_file() and entry.name.upper().endswith(".HEIC"):
+            print(f"🔄 Converting {entry.name}...")
+            heif_file = pillow_heif.read_heif(entry.absolute())
+            image = Image.frombytes(
+                heif_file.mode,
+                heif_file.size,
+                heif_file.data,
+                "raw",
+            )
+            file_name = f"{entry.stem}.png"
+            file_path = PurePath(output_dir, file_name)
+            image.save(file_path, format("png"))
+    
+    print("✅ Done")
+
+
+if __name__ == "__main__":
+    main()
+ 
